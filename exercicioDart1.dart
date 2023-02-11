550f9ca591e559100a089792fa523f61588f66fd
Leonardo Fontes <leeofv@gmail.com>
2023-02-11 14:09:58 -0300

    Exercicio em Dart

---
+++ exercicioDart1.dart
@@ -0,0 +1,26 @@
+void main() {
+    double valor1;
+    double valor2;
+
+    valor1 = 144;
+    valor2 = 2;
+   
+
+    double soma = valor1+valor2;
+    print("A soma entre o $valor1 e o $valor2 é: $soma");
+  
+    double subtracao = valor1-valor2;
+    print("A subtração entra o $valor1 e o $valor2 é: $subtracao");
+
+    double multiplicacao = valor1*valor2;
+    print("A multiplicação entre o $valor1 e o $valor2 é: $multiplicacao");
+
+    double divisao = valor1/valor2;
+    print("A divisão entre o $valor1 e o $valor2 é: $divisao");
+
+    double restoDivisao = valor1%valor2;
+    print("O resto da divisão entre o $valor1 e o $valor2 é: $restoDivisao");
+  
+    
+  
+}
