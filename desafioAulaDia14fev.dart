97d6d95611801e6cff6dcc374f8b703bcac6da7e
Leonardo Fontes <leeofv@gmail.com>
2023-02-15 17:25:44 -0300

    Desafio Aula do dia 14fev

---
+++ desafioAulaDia14fev.dart
@@ -0,0 +1,18 @@
+void main() {
+  String nomeAluno = "Gabriel";
+  double nota1 = 3;
+  double nota2 = 7;
+  double mediaEscola = 6;
+  String aprovado = "Aprovado";
+  String reprovado = "reprovado";
+  
+  double mediaAluno = (nota1+nota2)/2;
+  
+  print("A media do aluno $nomeAluno é $mediaAluno");
+  
+  if(mediaAluno >= mediaEscola) {
+    print("O aluno está $aprovado!");
+  }else {
+    print("O aluno está $reprovado!");
+  }
+}
