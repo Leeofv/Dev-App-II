385c3046ca148970312368b4c7b1a00b93219f3c
Leonardo Fontes <108199107+Leeofv@users.noreply.github.com>
2023-02-28 21:06:34 -0300

    Update desafio2.dart

--- aula16fev/desafio2.dart
+++ aula16fev/desafio2.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 
 void main() {
-  
-
   stdout.write("\nInforme os dados da pessoa \n Nome: ");
   String nome = stdin.readLineSync().toString();
 
@@ -15,17 +13,19 @@ void main() {
   stdout.write("Altura: ");
   double altura = double.parse(stdin.readLineSync().toString());
 
-  double pesoIdeal = 0;
+  dynamic pesoIdeal = 0;
 
   if (sexo == "M") {
     pesoIdeal = (72.7 * altura) - 58;
-  } else {
+  } else if (sexo == "F") {
     pesoIdeal = (62.1 * altura) - 44.7;
+  } else {
+    pesoIdeal = "Sexo inválido";
   }
   print("\n\nDados do cliente:\n"
       "Nome: $nome\n"
       "Idade: $idade\n"
       "Sexo: $sexo\n"
       "Altura: $altura\n"
-      "Peso Ideal: ${pesoIdeal.toStringAsFixed(2)}\n\n");
+      "Peso Ideal: $pesoIdeal\n\n");
 }
